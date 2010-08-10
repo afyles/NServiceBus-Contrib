e4d9009abb206da481059dff05f881a36821d5e9
andreasohlund <andreas_ohlund@hotmail.com>
2010-08-10 23:48:35 +0200

    Added top level directories

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("NServiceBus-Contrib")]
+[assembly: AssemblyCopyright("Copyright 2010 Udi Dahan, Andreas Ohlund, Matt Burton, et al. All rights reserved.")]
+[assembly: AssemblyTrademark("git unavailable")]
+[assembly: AssemblyVersion("0.1.0.0")]
+
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
+
