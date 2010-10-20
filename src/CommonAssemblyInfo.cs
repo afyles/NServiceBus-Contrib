dc1d32ae51c826dbd93494b4cb6b8fb550ae4564
Jens Pettersson <jens.pettersson@live.se>
2010-10-20 16:53:30 +0200

    Initial push for rakefiles.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,10 +1,12 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("NServiceBus-Contrib")]
+[assembly: AssemblyDescription("0.1.0.0")]
+[assembly: AssemblyProduct("NServiceBus-Contrib ")]
 [assembly: AssemblyCopyright("Copyright 2010 Udi Dahan, Andreas Ohlund, Matt Burton, et al. All rights reserved.")]
-[assembly: AssemblyTrademark("git unavailable")]
+[assembly: AssemblyTrademark("9509465f558fc1adb502871f2143e49754de5d2c")]
 [assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: AssemblyInformationalVersion("0.1.0.0")]
 
