d0a6ac28db07c4b0034fc40574baa609e0550391
Jens Pettersson <jens.pettersson@live.se>
2010-10-20 18:39:58 +0200

    The root rakefile now search for rakefiles in all subdirs of src and execute the build task

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("0.1.0.0")]
 [assembly: AssemblyProduct("NServiceBus-Contrib ")]
 [assembly: AssemblyCopyright("Copyright 2010 Udi Dahan, Andreas Ohlund, Matt Burton, et al. All rights reserved.")]
-[assembly: AssemblyTrademark("9509465f558fc1adb502871f2143e49754de5d2c")]
+[assembly: AssemblyTrademark("dc1d32ae51c826dbd93494b4cb6b8fb550ae4564")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
