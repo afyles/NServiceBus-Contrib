4bd8fe13dff588ec91e564e6f5ec7ef30551cdac
Mikael Nordberg <nordbergmikael@hotmail.com>
2010-10-21 11:23:06 +0200

    Added files

---
+++ src/SagaPersisters/RavenDB/NServiceBus.SagaPersisters.RavenDB/ConfigureRavenDBSagaPersister.cs
@@ -0,0 +1,37 @@
+﻿using log4net;
+using NServiceBus.ObjectBuilder;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace NServiceBus
+{
+    public static class ConfigureRavenDBSagaPersister
+    {
+        public static Configure EmbeddedRavenDBSagaPersister(this Configure config, string dataDirectory)
+        {
+            return config;
+        }
+
+        public static Configure RavenDBSagaPersister(this Configure config)
+        {
+            return RavenDBSagaPersister(config, "http://localhost:8080");
+        }
+
+        public static Configure RavenDBSagaPersister(this Configure config, string url)
+        {
+            if (!Sagas.Impl.Configure.SagasWereFound)
+                return config;
+
+            IDocumentStore documentStore = new DocumentStore { Url = url };
+            documentStore.Initialize();            
+          
+            config.Configurer.RegisterSingleton<IDocumentStore>(documentStore);
+            config.Configurer.ConfigureComponent<DocumentSessionFactory>(ComponentCallModelEnum.Singlecall);
+            config.Configurer.ConfigureComponent<SagaPersister>(ComponentCallModelEnum.Singlecall);
+
+            return config;
+        }
+
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(ConfigureRavenDBSagaPersister));
+    }
+}
---
+++ src/SagaPersisters/RavenDB/NServiceBus.SagaPersisters.RavenDB/RavenDBMessageModule.cs
@@ -0,0 +1,37 @@
+﻿using System.Transactions;
+
+namespace NServiceBus
+{
+    public class RavenDBMessageModule : IMessageModule
+    {
+        void IMessageModule.HandleBeginMessage()
+        {
+            if (DocumentSessionFactory == null) return;
+
+            DocumentSessionFactory.OpenSession();
+        }
+
+        void IMessageModule.HandleEndMessage()
+        {
+            if (DocumentSessionFactory == null) return;
+
+            DocumentSessionFactory.Current.SaveChanges();
+            DocumentSessionFactory.Complete();
+        }
+
+        void IMessageModule.HandleError()
+        {
+            if (DocumentSessionFactory == null) return;
+
+            if (Transaction.Current != null)
+            {
+                var txId = Transaction.Current.TransactionInformation.DistributedIdentifier;
+                DocumentSessionFactory.Current.Advanced.DatabaseCommands.Rollback(txId);
+            }
+
+            DocumentSessionFactory.Complete();
+        }
+
+        public IDocumentSessionFactory DocumentSessionFactory { get; set; }
+    }
+}
