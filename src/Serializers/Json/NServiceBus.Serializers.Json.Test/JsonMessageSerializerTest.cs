92985abd8c31fc9267d618e9860ad29726f7e381
Johannes <johannesgu@gmail.com>
2010-11-18 21:52:53 +0100

    Json serializer implementatin + tests

--- src/Serializers/Json/NServiceBus.Serializers.Json.Test/JsonMessageSerializerTest.cs
+++ src/Serializers/Json/NServiceBus.Serializers.Json.Test/JsonMessageSerializerTest.cs
@@ -1,26 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using InExchange.NServiceBus.Serializers;
-using InExchange.NServiceBus.Serializers.DoesNotWorkYet;
-using Newtonsoft.Json;
-using NServiceBus;
 using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
 using NUnit.Framework;
-//using SharpTestsEx;
 
-namespace InExchange.NServiceBus.Test
+namespace NServiceBus.Serializers.Json.Test
 {
   public class A : IMessage
   {
     public byte[] Data;
+    public string S;
+    public int I { get; set; }
   }
 
   public interface IA : IMessage
   {
+    byte[] Data { get; set; }
     string S { get; set; }
+    int I { get; set; }
+    B B { get; set; }
+  }
+
+  public class B
+  {
+    public string BS { get; set; }
   }
 
   [TestFixture]
@@ -29,9 +31,12 @@ namespace InExchange.NServiceBus.Test
     [Test]
     public void Test()
     {
-      var serializer = new JsonMessageSerializer();
+      var messageMapper = new MessageMapper();
+      messageMapper.Initialize(new[] { typeof(IA), typeof(A) });
 
-      var obj = new A() { Data = new byte[32] };
+      var serializer = new JsonMessageSerializer(messageMapper);
+
+      var obj = new A() { Data = new byte[32], I = 23, S = "Foo" };
 
       new Random().NextBytes(obj.Data);
 
@@ -47,44 +52,60 @@ namespace InExchange.NServiceBus.Test
 
       var result = serializer.Deserialize(output);
 
-      //result.Should()
-      //  .Not.Be.Null()
-      //  .And.Have.Count.EqualTo(1);
+      Assert.IsNotEmpty(result);
+      Assert.That(result, Has.Length.EqualTo(1));
 
-      //result[0].Should().Be.OfType<A>();
+      Assert.That(result[0], Is.TypeOf(typeof(A)));
+      var a = ((A)result[0]);
+
+      Assert.AreEqual(a.Data, obj.Data);
+      Assert.AreEqual(23, a.I);
+      Assert.AreEqual("Foo", a.S);
     }
 
     [Test]
     public void TestInterfaces()
     {
-      var serializer = new JsonMessageSerializer();
+      var messageMapper = new MessageMapper();
+      messageMapper.Initialize(new[] {typeof (IA), typeof (A)});
+
+      var serializer = new JsonMessageSerializer(messageMapper);
 
       var output = new MemoryStream();
 
-      var messageMapper = new MessageMapper();
-      messageMapper.Initialize(new [] { typeof(IA), typeof(A) });
+      var obj = messageMapper.CreateInstance<IA>(
+        x =>
+          {
+            x.S = "kalle";
+            x.I = 42;
+            x.Data = new byte[23];
+            x.B = new B {BS = "BOO"};
+          }
+        );
 
-      var obj = messageMapper.CreateInstance<IA>(x => x.S = "kalle");
-
-      var mappedType = messageMapper.GetMappedTypeFor(typeof (A));
-      var typeName = messageMapper.GetNewTypeName(typeof (A));
+      new Random().NextBytes(obj.Data);
 
       serializer.Serialize(new IMessage[] { obj }, output);
 
       output.Position = 0;
 
-      File.WriteAllBytes("json.txt", output.ToArray());
+      File.WriteAllBytes("json2.txt", output.ToArray());
 
       output.Position = 0;
 
       var result = serializer.Deserialize(output);
 
-      //result.Should()
-      //  .Not.Be.Null()
-      //  .And.Have.Count.EqualTo(1);
+      Assert.IsNotEmpty(result);
+      Assert.That(result, Has.Length.EqualTo(1));
 
-      //result[0].Should().Be.OfType<IA>();
-      //((IA) result[0]).S.Should().Be.EqualTo("kalle");
+      Assert.That(result[0], Is.AssignableTo(typeof(IA)));
+      var a = ((IA)result[0]);
+
+      Assert.AreEqual(a.Data, obj.Data);
+      Assert.AreEqual(42, a.I);
+      Assert.AreEqual("kalle", a.S);
+      Assert.IsNotNull(a.B);
+      Assert.AreEqual("BOO", a.B.BS);
     }
   }
 }
---
+++ src/Serializers/Json/README.markdown
@@ -0,0 +1,3 @@
+# NServiceBus Contrib - Json Serializer
+
+Original author: [Johannes Gustafsson](http://github.com/johannesg)
