2b7c04e7e0125c0311c1902da0323a284695932b
unknown <scul01@.Intelliflo.local>
2010-12-24 15:21:23 +0000

    created a quartz timeout manage

---
+++ src/TimeoutManagers/QuartzTimeoutManager/Timeout.Quartz/EndpointConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NServiceBus;
+using NServiceBus.Sagas.Impl;
+using Quartz;
+using Quartz.Impl;
+
+namespace Timeout.Quartz
+{
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server,
+        IWantCustomInitialization, IWantToRunAtStartup
+    {
+        void IWantCustomInitialization.Init()
+        {
+            //We don't want the saga message handler
+            //as it will continue to place timeout messages back on the bus
+            NServiceBus.Configure.With(AllTypesExcluding(new[] { typeof(SagaMessageHandler) }))
+                .DefaultBuilder()
+                .XmlSerializer()
+                .UnicastBus();
+
+            var schedulerFactory = new StdSchedulerFactory();
+
+            var configurer = NServiceBus.Configure.Instance.Configurer;
+            configurer.RegisterSingleton<TimeoutMessageDispatcherJob>(new TimeoutMessageDispatcherJob());
+            configurer.RegisterSingleton<ISchedulerFactory>(schedulerFactory);
+            configurer.RegisterSingleton<IScheduler>(schedulerFactory.GetScheduler());
+        }
+
+        public void Run()
+        {
+            var scheduler = NServiceBus.Configure.Instance.Builder.Build<IScheduler>();
+            scheduler.JobFactory = new BuilderJobFactory();
+            if(scheduler.IsStarted==false)
+                scheduler.Start();
+        }
+
+        public void Stop()
+        {
+            var scheduler = NServiceBus.Configure.Instance.Builder.Build<IScheduler>();
+            if(scheduler.IsShutdown==false)
+                scheduler.Shutdown();
+        }
+
+        private static IEnumerable<Type> AllTypesExcluding(IEnumerable<Type> excludeTypes)
+        {
+            var assemblies = NServiceBus.Configure.GetAssembliesInDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            return assemblies.SelectMany(assembly =>
+                                {
+                                    var types = new Type[] { };
+                                    try
+                                    {
+                                        types = assembly.GetTypes();
+                                    }
+                                    catch (ReflectionTypeLoadException) { }
+                                    return types;
+                                })
+                             .Where(type => excludeTypes.Contains(type)==false);
+        }
+    }
+}
---
+++ src/TimeoutManagers/QuartzTimeoutManager/Timeout.Quartz/TimeoutMessageHandler.cs
@@ -0,0 +1,49 @@
+﻿using NServiceBus;
+using NServiceBus.Saga;
+using Quartz;
+
+namespace Timeout.Quartz
+{
+    public class TimeoutMessageHandler : IHandleMessages<TimeoutMessage>
+    {
+        public IBus Bus { get; set; }
+        public IScheduler Scheduler { get; set; }
+
+        private const string GroupName = "TimeOut";
+
+        public void Handle(TimeoutMessage message)
+        {
+            if (message.ClearTimeout)
+            {
+                Scheduler.UnscheduleJob(GetTriggerName(message), GroupName);
+            }
+            else if (message.HasNotExpired())
+            {
+                var jobDetail = new JobDetail(GetJobName(message), GroupName, typeof(TimeoutMessageDispatcherJob));
+                jobDetail.JobDataMap.Add(TimeoutMessageDispatcherJob.ReturnAddressKey, Bus.CurrentMessageContext.ReturnAddress);
+                jobDetail.JobDataMap.Add(TimeoutMessageDispatcherJob.ClearTimeoutKey, message.ClearTimeout);
+                jobDetail.JobDataMap.Add(TimeoutMessageDispatcherJob.ExpiresKey, message.Expires);
+                jobDetail.JobDataMap.Add(TimeoutMessageDispatcherJob.SagaIdkey, message.SagaId);
+                jobDetail.JobDataMap.Add(TimeoutMessageDispatcherJob.StateKey, message.State);
+
+                Trigger trigger = new SimpleTrigger(GetTriggerName(message), GroupName, message.Expires);
+
+                Scheduler.ScheduleJob(jobDetail, trigger);
+            }
+            else
+            {
+                Bus.Send(Bus.CurrentMessageContext.ReturnAddress, new IMessage[] { message });
+            }
+        }
+
+        private static string GetTriggerName(TimeoutMessage message)
+        {
+            return "TimeoutJob-Trigger: " + message.SagaId;
+        }
+
+        private static string GetJobName(TimeoutMessage message)
+        {
+            return "TimeoutJob-JobDetail: " + message.SagaId;
+        }
+    }
+}
