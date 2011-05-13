b04513c7a2454790172b36ca0a60e1d67641aca3
afyles <afyles@gmail.com>
2011-05-12 20:39:16 -0400

    Added relative path entry, saving path state, and debub options

--- src/VSTemplates/Wizard/CollectUserInputForm.Designer.cs
+++ src/VSTemplates/Wizard/CollectUserInputForm.Designer.cs
@@ -34,11 +34,18 @@
             this.finishButton = new System.Windows.Forms.Button();
             this.instructionsLabel = new System.Windows.Forms.Label();
             this.pathLabel = new System.Windows.Forms.Label();
+            this.relPathTextBox = new System.Windows.Forms.TextBox();
+            this.fullPathBox = new System.Windows.Forms.GroupBox();
+            this.relativePathBox = new System.Windows.Forms.GroupBox();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.fullPathBox.SuspendLayout();
+            this.relativePathBox.SuspendLayout();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // selectButton
             // 
-            this.selectButton.Location = new System.Drawing.Point(190, 4);
+            this.selectButton.Location = new System.Drawing.Point(6, 23);
             this.selectButton.Name = "selectButton";
             this.selectButton.Size = new System.Drawing.Size(75, 23);
             this.selectButton.TabIndex = 0;
@@ -48,7 +55,7 @@
             // 
             // finishButton
             // 
-            this.finishButton.Location = new System.Drawing.Point(109, 119);
+            this.finishButton.Location = new System.Drawing.Point(100, 204);
             this.finishButton.Name = "finishButton";
             this.finishButton.Size = new System.Drawing.Size(75, 23);
             this.finishButton.TabIndex = 1;
@@ -59,33 +66,83 @@
             // instructionsLabel
             // 
             this.instructionsLabel.AutoSize = true;
-            this.instructionsLabel.Location = new System.Drawing.Point(12, 9);
+            this.instructionsLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.instructionsLabel.Location = new System.Drawing.Point(0, 0);
+            this.instructionsLabel.Margin = new System.Windows.Forms.Padding(3);
             this.instructionsLabel.Name = "instructionsLabel";
-            this.instructionsLabel.Size = new System.Drawing.Size(172, 13);
+            this.instructionsLabel.Padding = new System.Windows.Forms.Padding(3);
+            this.instructionsLabel.Size = new System.Drawing.Size(239, 19);
             this.instructionsLabel.TabIndex = 2;
-            this.instructionsLabel.Text = "Select the path to the NSB libraries";
+            this.instructionsLabel.Text = "Select or enter the Hint path to the NSB libraries";
             // 
             // pathLabel
             // 
-            this.pathLabel.AutoSize = true;
-            this.pathLabel.Location = new System.Drawing.Point(12, 38);
+            this.pathLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pathLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pathLabel.Location = new System.Drawing.Point(5, 129);
             this.pathLabel.Name = "pathLabel";
-            this.pathLabel.Size = new System.Drawing.Size(123, 13);
+            this.pathLabel.Padding = new System.Windows.Forms.Padding(5);
+            this.pathLabel.Size = new System.Drawing.Size(274, 45);
             this.pathLabel.TabIndex = 3;
             this.pathLabel.Text = "/Path/To/NSB/Libraries";
+            // 
+            // relPathTextBox
+            // 
+            this.relPathTextBox.Location = new System.Drawing.Point(6, 24);
+            this.relPathTextBox.Name = "relPathTextBox";
+            this.relPathTextBox.Size = new System.Drawing.Size(245, 20);
+            this.relPathTextBox.TabIndex = 4;
+            this.relPathTextBox.TextChanged += new System.EventHandler(this.relPathTextBox_TextChanged);
+            // 
+            // fullPathBox
+            // 
+            this.fullPathBox.Controls.Add(this.selectButton);
+            this.fullPathBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.fullPathBox.Location = new System.Drawing.Point(5, 5);
+            this.fullPathBox.Name = "fullPathBox";
+            this.fullPathBox.Size = new System.Drawing.Size(274, 62);
+            this.fullPathBox.TabIndex = 5;
+            this.fullPathBox.TabStop = false;
+            this.fullPathBox.Text = "Full Path";
+            // 
+            // relativePathBox
+            // 
+            this.relativePathBox.Controls.Add(this.relPathTextBox);
+            this.relativePathBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.relativePathBox.Location = new System.Drawing.Point(5, 67);
+            this.relativePathBox.Name = "relativePathBox";
+            this.relativePathBox.Size = new System.Drawing.Size(274, 62);
+            this.relativePathBox.TabIndex = 6;
+            this.relativePathBox.TabStop = false;
+            this.relativePathBox.Text = "Relative Path";
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.pathLabel);
+            this.mainPanel.Controls.Add(this.relativePathBox);
+            this.mainPanel.Controls.Add(this.fullPathBox);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mainPanel.Location = new System.Drawing.Point(0, 19);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Padding = new System.Windows.Forms.Padding(5);
+            this.mainPanel.Size = new System.Drawing.Size(284, 179);
+            this.mainPanel.TabIndex = 7;
             // 
             // CollectUserInputForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 154);
-            this.Controls.Add(this.pathLabel);
+            this.ClientSize = new System.Drawing.Size(284, 236);
+            this.Controls.Add(this.mainPanel);
             this.Controls.Add(this.instructionsLabel);
             this.Controls.Add(this.finishButton);
-            this.Controls.Add(this.selectButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CollectUserInputForm";
             this.Text = "NSB Template Wizard";
+            this.fullPathBox.ResumeLayout(false);
+            this.relativePathBox.ResumeLayout(false);
+            this.relativePathBox.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +155,9 @@
         private System.Windows.Forms.Button finishButton;
         private System.Windows.Forms.Label instructionsLabel;
         private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox relPathTextBox;
+        private System.Windows.Forms.GroupBox fullPathBox;
+        private System.Windows.Forms.GroupBox relativePathBox;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
--- src/VSTemplates/Wizard/CollectUserInputForm.cs
+++ src/VSTemplates/Wizard/CollectUserInputForm.cs
@@ -1,16 +1,34 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace NServiceBus.Contrib.Templates.Wizard
 {
     public partial class CollectUserInputForm : Form
     {
+        private const String AppDataSubDirectory = "NServiceBus\\Templates";
+        private const String TemplateStateFileName = "state.xml";
+        private const String LastLibraryPathNodeName = "LastLibraryPath";
+        private String templateStateFilePath;
+        private String appDataDirectory;
         private String libPath;
 
         public CollectUserInputForm()
         {
             InitializeComponent();
+
+            this.appDataDirectory = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                AppDataSubDirectory);
+
+            this.templateStateFilePath = Path.Combine(
+                this.appDataDirectory,
+                TemplateStateFileName);
+
+            this.LoadLastPath();
         }
+
 
         public String LibraryPath
         {
@@ -30,7 +48,52 @@ namespace NServiceBus.Contrib.Templates.Wizard
 
         private void finishButton_Click(object sender, EventArgs e)
         {
+            this.SaveLastPath();
             this.Dispose();
+        }
+
+        private void relPathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            this.libPath = this.relPathTextBox.Text;
+            this.pathLabel.Text = this.libPath;
+        }
+
+
+        private void LoadLastPath()
+        {
+            if (File.Exists(this.templateStateFilePath))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(this.templateStateFilePath);
+                XmlNode lastLibPathNode = doc.SelectSingleNode(LastLibraryPathNodeName);
+
+                if (null != lastLibPathNode)
+                {
+                    this.libPath = lastLibPathNode.InnerText;
+                    this.pathLabel.Text = this.libPath;
+                }
+            }
+        }
+
+        private void SaveLastPath()
+        {
+            try
+            {
+                if (!Directory.Exists(this.appDataDirectory))
+                    Directory.CreateDirectory(this.appDataDirectory);
+
+                XmlDocument doc = new XmlDocument();
+                XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", null, null);
+                doc.AppendChild(declaration);
+                XmlElement libPath = doc.CreateElement(LastLibraryPathNodeName);
+                libPath.InnerText = this.libPath;
+                doc.AppendChild(libPath);
+                doc.Save(this.templateStateFilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("{0}: {1}","There was an error saving the last path chosen", ex.Message), "Error", MessageBoxButtons.OK);
+            }
         }
     }
 }
